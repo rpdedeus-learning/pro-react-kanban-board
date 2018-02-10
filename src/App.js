4c87b0d318682e1d16f10b8b22d990a047251141
Roberto DeDeus <roberto.p.dedeus@gmail.com>
2018-02-09 20:35:01 -0500

    Build components.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,56 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import KanbanBoard from './KanbanBoard';
+
+let cardlist = [
+  {
+    id: 1,
+    title: "Read the Book",
+    description: "I should react the whole book",
+    status: "in-progress",
+    tasks: []
+  },
+  {
+    id: 2,
+    title: "Write some code",
+    description: "Code along with the samples in the book",
+    status: "todo",
+    tasks: [
+      {
+        id: 1,
+        name: "ContactList Example",
+        done: true
+      },
+      {
+        id: 2,
+        name: "Kanban Example",
+        done: false
+      },
+      {
+        id: 3,
+        name: "My own experiements",
+        done: false
+      }
+    ]
+  }
+]
 
 class App extends Component {
+
   render() {
+
     return (
       <div className="App">
-        <header className="App-header">
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        </p> */}
+
+        <KanbanBoard cards={cardlist} />
       </div>
     );
   }
---
+++ src/CheckList.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+
+class CheckList extends Component {
+    
+    render() {
+
+        let tasks = this.props.tasks.map((task) => (
+            <li className="checklist_task">
+                <input type="checkbox" defaultChecked={task.done} />
+                {task.name}
+                <a href="#" className="checklist_task--remove" />
+            </li>
+        ));
+
+        return (
+            <div className="checkList">
+                <ul>{tasks}</ul>
+            </div>
+        );
+    }
+}
+export default CheckList;
