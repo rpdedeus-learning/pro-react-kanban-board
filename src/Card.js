0c843ed39756e4a3079b0948822abed9d6960447
Roberto DeDeus <roberto.p.dedeus@gmail.com>
2018-02-09 21:32:38 -0500

    Add event handler to Card component.

--- src/Card.js
+++ src/Card.js
@@ -2,16 +2,33 @@ import React, {Component} from 'react';
 import CheckList from './CheckList';
 
 class Card extends Component {
-    
+
+    constructor() {
+
+        super(...arguments);
+        this.state = {
+            showDetails: false
+        };
+    }
+
     render() {
 
-        return (
-            <div className="card">
-                <div className="card_title">{this.props.title}</div>
+        let cardDetails;
+        if (this.state.showDetails) {
+            cardDetails = (
                 <div className="card_details">
                     {this.props.description}
                     <CheckList cardId={this.props.cardId} tasks={this.props.tasks} />
                 </div>
+            );
+        };
+
+        return (
+            <div className="card">
+                <div className="card_title" onClick={()=> this.setState({showDetails: !this.state.showDetails})}>
+                    {this.props.title}
+                </div>
+                {cardDetails}
             </div>
         );
     }
