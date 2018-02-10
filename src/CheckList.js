137fb02b82dd12844535673380dfec1d0e7dc190
Roberto DeDeus <roberto.p.dedeus@gmail.com>
2018-02-10 15:28:55 -0500

    Add keys.

--- src/CheckList.js
+++ src/CheckList.js
@@ -1,11 +1,11 @@
 import React, {Component} from 'react';
 
 class CheckList extends Component {
-    
+
     render() {
 
         let tasks = this.props.tasks.map((task) => (
-            <li className="checklist_task">
+            <li key={task.id} className="checklist_task">
                 <input type="checkbox" defaultChecked={task.done} />
                 {task.name}
                 <a href="#" className="checklist_task--remove" />
@@ -15,6 +15,7 @@ class CheckList extends Component {
         return (
             <div className="checkList">
                 <ul>{tasks}</ul>
+                <input type="text" className="checkList--add-task" placeholder="Type then hit enter to add task" />
             </div>
         );
     }
