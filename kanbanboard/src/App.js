ac293fe7d8040ada77e30c2c1671cfcf0fa8deea
roberto.p.dedeus <roberto.p.dedeus@gmail.com>
2018-02-22 20:52:00 -0500

    Fetch data from external API.

--- kanbanboard/src/App.js
+++ kanbanboard/src/App.js
@@ -1,50 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import KanbanBoard from './KanbanBoard';
-
-let cardlist = [
-  {
-    id: 1,
-    title: "Read the Book",
-    description: "I should react the **whole** book",
-    color: "#BD8D31",
-    status: "in-progress",
-    tasks: []
-  },
-  {
-    id: 2,
-    title: "Write some code",
-    description: "Code along with the samples in the book. The complete source can be found at [github](https://github.com/pro-react/)",
-    status: "todo",
-    color: "#3A7E28",
-    tasks: [
-      {
-        id: 1,
-        name: "ContactList Example",
-        done: true
-      },
-      {
-        id: 2,
-        name: "Kanban Example",
-        done: false
-      },
-      {
-        id: 3,
-        name: "My own experiements",
-        done: false
-      }
-    ]
-  },
-  {
-    id: 3,
-    title: "A new card with a super long description that is much longer than 80 characters. This will certainly fail the validation for this property.",
-    description: "",
-    status: "todo",
-    color: "",
-    tasks: []
-  }
-]
+import KanbanBoardContainer from './KanbanBoardContainer';
 
 class App extends Component {
 
@@ -52,15 +9,7 @@ class App extends Component {
 
     return (
       <div className="App">
-        {/* <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p> */}
-
-        <KanbanBoard cards={cardlist} />
+        <KanbanBoardContainer />
       </div>
     );
   }
---
+++ kanbanboard/src/KanbanBoardContainer.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import KanbanBoard from './KanbanBoard';
+import 'whatwg-fetch';
+
+const API_URL = 'http://kanbanapi.pro-react.com';
+const API_HEADERS = {
+    'Content-Type': 'application:json',
+    Authorization: 'This application is a special snow flake and there is no other like it'
+};
+
+class KanbanBoardContainer extends Component {
+
+    constructor() {
+
+        super(...arguments)
+        this.state = {
+            cards: [],
+        };
+    }
+
+    componentDidMount() {
+        fetch(API_URL+'/cards/', {headers: API_HEADERS})
+        .then((response) => response.json())
+        .then((responseData) => {
+            this.setState({cards: responseData});
+        })
+        .catch((error) => {
+            console.log('Error fetching and parsing data', error);
+        });
+    }
+ 
+    render() {
+
+        return (
+            <KanbanBoard cards={this.state.cards} />
+        )
+    }
+
+}
+
+export default KanbanBoardContainer;
